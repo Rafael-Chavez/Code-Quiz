5c0af3308d183e2648535c4333bcbc2d6bd27b01
Rafael Chavez <chavezrafaelbw@gmail.com>
2021-02-25 02:04:48 -0500

    added the question script along with questions variable.

---
+++ questions.js
@@ -0,0 +1,39 @@
+// Here we are making an array of object of questions that will be listed
+
+var questions = [
+    {
+        title: "Which country has the biggest population?",
+        choices: ["japan", "India", "Mexico", "USA"],
+        answer: "India"
+    },
+
+    {
+        title: "What color are school busses usually?",
+        choices: ["red", "yellow", "pink", "blue"],
+        answer: "yellow"
+    },
+
+    {
+        title: "What is H20?",
+        choices: ["water","chicken","taco","iron"],
+        answer: "water"
+    },
+
+    {
+        title: "how many licks does it take to get to the tootsie roll center of a tootsie pop",
+        choices: ["one","infinity","7465","never"],
+        answer: "7465"
+    },
+
+    {
+        title: "is doing all your homework last minute a bad idea?",
+        choices: ["yes", "no", "Of course, get right on it!"],
+        answer: "Of course, get right on it!"
+    },
+
+    {
+        title: "this is free just put three",
+        choices: ["three", "rock", "paper", "scissors"],
+        answer: "three"
+    },
+]
