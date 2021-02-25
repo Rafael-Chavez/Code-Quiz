f07ab64e57f886a93c1c3d8921ddae4589ef367c
Rafael Chavez <chavezrafaelbw@gmail.com>
2021-02-25 02:24:51 -0500

    added more to the script js to start applying functions to the timer and the questions.

--- script.js
+++ script.js
@@ -2,9 +2,41 @@ var questionsAsked = 0;
 var time = questions.length * 10;
 var timerId;
 
-var currentTime = document.getElementById("");
-var timer = document.getElementById("");
-var choices = document.getElementById("");
-var submitButton = document.getElementById("");
-var startButton = document.getElementById("");
-var initials = document.getElementById("");
+var questionsEl = document.getElementById("questions")
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitButton = document.getElementById("submit");
+var startButton = document.getElementById("start");
+var initialsEl = document.getElementById("init");
+
+function startQuiz() {
+
+    var startScreenEl = document.getElementById("start-screen");
+    startScreenEl.setAttribute("class", "hide");
+
+    questions.removeAttribute("class");
+
+    timerId = setInterval(clockTick, 1000);
+
+    currentTime.textContent = time;
+
+    getQuestions();
+
+    var currentQuestion = questions[currentQuestionIndex];
+    var titleEl = document.getElementById("question-title");
+    titleEl.textContent = currentQuestion.title;
+    choicesEl.innerHTML = "";
+
+    currentQuestion.choices.forEach(function(choice, i) {
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "choice");
+        choiceNode.setAttribute("value", choice);
+
+        choiceNode.textContent = i + 1 + ". " + choice;
+
+
+        choiceNode.onClick = questionClick;
+
+        choicesEl.appendChild(choiceNode);
+    });
+}
